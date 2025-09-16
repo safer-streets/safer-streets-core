89367789607d364167cfc488afa8ee0ddccd7d1e
virgesmith <a.p.smith@leeds.ac.uk>
2025-09-16 17:07:15 +0100

    fix

--- src/safer_streets_core/measures.py
+++ src/safer_streets_core/measures.py
@@ -19,7 +19,7 @@ def lorenz_curve(
     else:
         data = data_in[[data_col, weight_col]]
     data["order"] = data[data_col] / data[weight_col]
-    data = data.sort_values(by=["order", "count"], ascending=False).cumsum().set_index(weight_col, drop=True)[data_col]
+    data = data.sort_values(by=["order", data_col], ascending=False).cumsum().set_index(weight_col, drop=True)[data_col]
     # add origin
     data.loc[0.0] = 0.0
     # return pd.Series(index=1.0 - data.index / data.index.max(), data=data[data_col]).sort_index()
